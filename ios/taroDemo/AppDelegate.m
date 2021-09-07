1921c2d07130b9541f392c80459aa68debae8652
jihan <2529699061@qq.com>
2021-09-07 16:21:56 +0800

    add production

--- ios/taroDemo/AppDelegate.m
+++ ios/taroDemo/AppDelegate.m
@@ -73,9 +73,10 @@ static void InitializeFlipper(UIApplication *application) {
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+return [NSURL URLWithString:[[NSBundle mainBundle] pathForResource:@"index.jsbundle" ofType:nil]];
+  
 #endif
 }
 
